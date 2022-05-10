ab549a6163114f7fee701c93658e0f75e3c84e9a
Vesa Nieminen <vesa@vaadin.com>
2022-05-10 12:07:19 +0300

    Added Fontawesome addon.

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,10 +1,10 @@
 package com.example.application.views.helloworld;
 
 import com.example.application.views.MainLayout;
+import com.flowingcode.vaadin.addons.fontawesome.FontAwesome;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -12,22 +12,13 @@ import com.vaadin.flow.router.RouteAlias;
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
+@Uses(FontAwesome.Regular.Icon.class)
+@Uses(FontAwesome.Solid.Icon.class)
 public class HelloWorldView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
-
     public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        add(new Button(FontAwesome.Regular.ADDRESS_CARD.create()));
+        add(new Button(FontAwesome.Solid.ADDRESS_BOOK.create()));
     }
 
 }
